4e7c2d0595bbecd7d95d0cc5c07c7d3826406228
zhuemann <56049645+zhuemann@users.noreply.github.com>
2023-12-12 12:25:33 -0600

    Running contexutal net with PET language model with late unfreezing

--- train_segmentation.py
+++ train_segmentation.py
@@ -75,7 +75,7 @@ def train_image_text_segmentation(config, batch_size=8, epoch=1, dir_base = "/ho
     # model specific global variables
     IMG_SIZE = config["IMG_SIZE"] #256 #1024 #512 #384
     #BATCH_SIZE = batch_size
-    LR = 3e-5 #1e-4 #5e-5 #5e-5 was lr for contextualnet runs #8e-5  # 1e-4 was for efficient #1e-06 #2e-6 1e-6 for transformer 1e-4 for efficientnet
+    LR = 5e-5 #1e-4 #5e-5 #5e-5 was lr for contextualnet runs #8e-5  # 1e-4 was for efficient #1e-06 #2e-6 1e-6 for transformer 1e-4 for efficientnet
     #LR = 5e-4
     N_EPOCHS = epoch
     N_CLASS = n_classes
@@ -123,8 +123,8 @@ def train_image_text_segmentation(config, batch_size=8, epoch=1, dir_base = "/ho
     #ngram_synonom = pd.read_excel(ngram_synonom_path, engine='openpyxl')
 
 
-    bert_path = os.path.join(dir_base, 'Zach_Analysis/models/rad_bert/')
-    #bert_path = os.path.join(dir_base, 'Zach_Analysis/models/roberta_pretrained_v3')
+    #bert_path = os.path.join(dir_base, 'Zach_Analysis/models/rad_bert/')
+    bert_path = os.path.join(dir_base, 'Zach_Analysis/models/roberta_pretrained_v3')
     tokenizer = AutoTokenizer.from_pretrained(bert_path)
     language_model = RobertaModel.from_pretrained(bert_path, output_hidden_states=True)
     #language_model = None
