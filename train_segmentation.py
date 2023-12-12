a35054a64ff5d281ea70b1fb26d2ddb9203ac98a
zhuemann <56049645+zhuemann@users.noreply.github.com>
2023-12-12 16:32:55 -0600

    Running contexutal net with radbert frozen no augmentation

--- train_segmentation.py
+++ train_segmentation.py
@@ -226,18 +226,18 @@ def train_image_text_segmentation(config, batch_size=8, epoch=1, dir_base = "/ho
     if using_t5:
         albu_augs = albu.Compose([
             #albu.RandomCrop(height = 256, width=256, always_apply=True),    #take out later
-            """albu.OneOf([
-                albu.RandomContrast(),
-                albu.RandomGamma(),
-                albu.RandomBrightness(),
-                       ], p=.3),
-            albu.OneOf([
-                albu.GridDistortion(),
-                albu.OpticalDistortion(distort_limit=2, shift_limit=0.5),
-                albu.ElasticTransform(alpha=120, sigma=120 * 0.05, alpha_affine=120 * 0.03)
-            ], p=.3),
-            albu.ShiftScaleRotate()
-            """
+            #albu.OneOf([
+            #    albu.RandomContrast(),
+            #    albu.RandomGamma(),
+            #    albu.RandomBrightness(),
+            #           ], p=.3),
+            #albu.OneOf([
+            #    albu.GridDistortion(),
+            #    albu.OpticalDistortion(distort_limit=2, shift_limit=0.5),
+            #    albu.ElasticTransform(alpha=120, sigma=120 * 0.05, alpha_affine=120 * 0.03)
+            #], p=.3),
+            #albu.ShiftScaleRotate()
+
         ])
 
 
