61ef5ed93376dda8bef50748a90995fa4ca2f577
zhuemann <56049645+zhuemann@users.noreply.github.com>
2023-12-13 13:44:58 -0600

    Running contexutal net vision with capped image and distortions with shiftscalerotate only

--- train_segmentation.py
+++ train_segmentation.py
@@ -233,12 +233,12 @@ def train_image_text_segmentation(config, batch_size=8, epoch=1, dir_base = "/ho
             #    albu.RandomGamma(),
             #    albu.RandomBrightness(),
             #           ], p=.3),
-            #albu.OneOf([
-            #    albu.GridDistortion(),
-            #    albu.OpticalDistortion(distort_limit=2, shift_limit=0.5),
-            #    albu.ElasticTransform(alpha=120, sigma=120 * 0.05, alpha_affine=120 * 0.03)
-            #], p=.3),
-            #albu.ShiftScaleRotate()
+            albu.OneOf([
+                albu.GridDistortion(),
+                albu.OpticalDistortion(distort_limit=2, shift_limit=0.5),
+                albu.ElasticTransform(alpha=120, sigma=120 * 0.05, alpha_affine=120 * 0.03)
+            ], p=.3),
+            albu.ShiftScaleRotate()
 
         ])
 
