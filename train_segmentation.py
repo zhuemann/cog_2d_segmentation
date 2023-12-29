06fd4ab7a386bc62c906d67de571e10ea31dc72c
zhuemann <56049645+zhuemann@users.noreply.github.com>
2023-12-29 01:30:02 -0600

    first run with new mip labels

--- train_segmentation.py
+++ train_segmentation.py
@@ -463,7 +463,6 @@ def train_image_text_segmentation(config, batch_size=8, epoch=1, dir_base = "/ho
             targets = torch.squeeze(targets)
             images = data['images'].to(device, dtype=torch.float)
 
-
             #print(targets.sum())
             #outputs = test_obj(images, ids, mask)  # for lavt
             outputs = test_obj(images, ids, mask, token_type_ids)
