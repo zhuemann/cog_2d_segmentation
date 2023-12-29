a9461d3b9b2ca1e623543886505dc4492db39bb0
zhuemann <56049645+zhuemann@users.noreply.github.com>
2023-12-28 23:50:49 -0600

    Running with cropped saggital vision model

--- train_segmentation.py
+++ train_segmentation.py
@@ -463,6 +463,7 @@ def train_image_text_segmentation(config, batch_size=8, epoch=1, dir_base = "/ho
             targets = torch.squeeze(targets)
             images = data['images'].to(device, dtype=torch.float)
 
+            print(targets.sum())
             #outputs = test_obj(images, ids, mask)  # for lavt
             outputs = test_obj(images, ids, mask, token_type_ids)
             #outputs = test_obj(images)
