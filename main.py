213873075039a4fc0805d6ea2c050810a15fce02
zhuemann <56049645+zhuemann@users.noreply.github.com>
2024-03-09 15:00:36 -0600

    contextual net lower learning rate more augs v4

--- main.py
+++ main.py
@@ -55,7 +55,7 @@ if __name__ == '__main__':
 
         #folder_name = "cropped_mips/baseline_nnunet_v17/seed" + str(seed) + "/"
         #save_string = "/UserData/Zach_Analysis/result_logs/cog_mip_segmentation/initial_testing/" + folder_name
-        folder_name = "/contextual_net_lower_lr_v3/"
+        folder_name = "/contextual_net_lower_lr_more_augs_v4/"
         save_string = "/UserData/Zach_Analysis/result_logs/visual_grounding/using_mips/initial_testing/" + folder_name
         save_location = os.path.join(directory_base, save_string)
         # save_location = ""
