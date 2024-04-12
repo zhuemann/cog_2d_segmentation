1fe5aa6094638f02094093ff997f1cfbb3b6f05e
zhuemann <56049645+zhuemann@users.noreply.github.com>
2024-04-12 17:10:23 -0500

    testing pipeline with llms extraction

--- data_prepocessing/data_pipeline.py
+++ data_prepocessing/data_pipeline.py
@@ -5,11 +5,14 @@ from data_prepocessing.concenus_voting import concenus_voting
 import pandas as pd
 def run_data_pipeline():
 
+    save_base = "/UserData/Zach_Analysis/suv_slice_text/uw_lymphoma_preprocess_chain_v3/"
+
     df = split_sentences()
-    #print(df)
-    #print(fail)
+    df.to_excel(save_base + "sentences_split_1.xlsx", index=False)
+
     df = llm_slice_suv_extraction(df)
-    #df.to_excel("model_predictions_for_suv_slice_extraction.xlsx", index=False, sheet_name='Predictions')
-    #df = pd.read_excel("test.xlsx")
+    df.to_excel(save_base + "model_predictions_for_suv_slice_extraction_2.xlsx", index=False, sheet_name='Predictions')
+
+    #df = pd.read_excel(save_base + "model_predictions_for_suv_slice_extraction_2.xlsx")
     df = concenus_voting(df)
-    print(df)
+    df.to_excel(save_base + "concenus_output_3.xlsx", index=False)
