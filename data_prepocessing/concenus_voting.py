4191e90cd7c4e8451fa7626a27e13b54ea6a2c69
zhuemann <56049645+zhuemann@users.noreply.github.com>
2024-04-12 16:56:41 -0500

    testing pipeline with llms extraction

--- data_prepocessing/concenus_voting.py
+++ data_prepocessing/concenus_voting.py
@@ -1,5 +1,5 @@
 from collections import Counter
-
+import pandas as pd
 def concenus_voting_old(df):
 
     num_3s = 0
@@ -102,57 +102,10 @@ def concenus_voting(df):
         slice_count = Counter(slice_list)
         suv_count = Counter(suv_list)
 
+        slice_num = max(slice_count, key=suv_count.get)
+        suv_num = max(suv_count, key=suv_count.get)
         print(slice_count)
         print(suv_count)
-
-        agree_slice = row["num_agree_slice"]
-        agree_suv = row["num_agree_suv"]
-
-        if agree_slice == 3:
-            num_3s += 1
-            slice_num = row["Slice"]
-        if agree_suv == 3:
-            suv_num = row["SUV"]
-            num_3s += 1
-        if agree_slice == 1:
-            num_1s += 1
-            if row["rules_mixtral_slice"] == 1:
-                slice_num = row["Slice"]
-            elif row["rules_mistral_slice"] == 1:
-                slice_num = row["Slice"]
-            else:
-                slice_num = row["AI_Slice"]
-        if agree_suv == 1:
-            num_1s += 1
-            if row["rules_mixtral_suv"] == 1:
-                suv_num = row["SUV"]
-            elif row["rules_mistral_suv"] == 1:
-                suv_num = row["SUV"]
-            else:
-                suv_num = row["AI_SUV"]
-        if agree_slice == 0:
-            num_0s += 1
-            continue
-            print(row["Extracted Sentences"])
-            slice_num = row["Slice"]
-            num_0s += 1
-        if agree_suv == 0:
-            num_0s += 1
-            continue
-            print(row["Extracted Sentences"])
-            suv_num = row["SUV"]
-            num_0s += 1
-
-        if suv_num < 2.5:
-            suv_cut += 1
-            continue
-
-        if contains_target_phrases(row["Extracted Sentences"]):
-            templated_background += 1
-            # print(row["Extracted Sentences"])
-            # print(f"slice: {slice_num}")
-            # print(f"suv_num: {suv_num}")
-            continue
 
         new_df.append({"Petlymph": row["Petlymph"],
                        "Findings": row["Findings"],
