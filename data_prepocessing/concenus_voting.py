a9937262d483df22ed96cc932380f504e469b72d
zhuemann <56049645+zhuemann@users.noreply.github.com>
2024-04-12 16:42:37 -0500

    testing pipeline with llms extraction

--- data_prepocessing/concenus_voting.py
+++ data_prepocessing/concenus_voting.py
@@ -1,3 +1,81 @@
+from collections import Counter
+
+def concenus_voting_old(df):
+
+    num_3s = 0
+    num_1s = 0
+    num_0s = 0
+    new_df = []
+    suv_cut = 0
+    templated_background = 0
+    for index, row in df.iterrows():
+
+        agree_slice = row["num_agree_slice"]
+        agree_suv = row["num_agree_suv"]
+
+        if agree_slice == 3:
+            num_3s += 1
+            slice_num = row["Slice"]
+        if agree_suv == 3:
+            suv_num = row["SUV"]
+            num_3s += 1
+        if agree_slice == 1:
+            num_1s += 1
+            if row["rules_mixtral_slice"] == 1:
+                slice_num = row["Slice"]
+            elif row["rules_mistral_slice"] == 1:
+                slice_num = row["Slice"]
+            else:
+                slice_num = row["AI_Slice"]
+        if agree_suv == 1:
+            num_1s += 1
+            if row["rules_mixtral_suv"] == 1:
+                suv_num = row["SUV"]
+            elif row["rules_mistral_suv"] == 1:
+                suv_num = row["SUV"]
+            else:
+                suv_num = row["AI_SUV"]
+        if agree_slice == 0:
+            num_0s += 1
+            continue
+            print(row["Extracted Sentences"])
+            slice_num = row["Slice"]
+            num_0s += 1
+        if agree_suv == 0:
+            num_0s += 1
+            continue
+            print(row["Extracted Sentences"])
+            suv_num = row["SUV"]
+            num_0s += 1
+
+        if suv_num < 2.5:
+            suv_cut += 1
+            continue
+
+        if contains_target_phrases(row["Extracted Sentences"]):
+            templated_background += 1
+            # print(row["Extracted Sentences"])
+            # print(f"slice: {slice_num}")
+            # print(f"suv_num: {suv_num}")
+            continue
+
+        new_df.append({"Petlymph": row["Petlymph"],
+                       "Findings": row["Findings"],
+                       "Impression": row["Impression"],
+                       "Extracted Sentences": row["Extracted Sentences"],
+                       "Slice": slice_num,
+                       "SUV": suv_num
+                       })
+
+    # Convert list to DataFrame and save
+    output_df = pd.DataFrame(new_df)
+    #output_df.to_excel('concensus_slice_suv_anonymized.xlsx', index=False)
+    print(f"num 3: {num_3s}")
+    print(f"num 1: {num_1s}")
+    print(f"num 0: {num_0s}")
+    print(f"below 2.5: {suv_cut}")
+    print(f"templated lang: {templated_background}")
+    return output_df
 
 def concenus_voting(df):
 
@@ -8,6 +86,24 @@ def concenus_voting(df):
     suv_cut = 0
     templated_background = 0
     for index, row in df.iterrows():
+
+
+        slice_list = []
+        suv_list = []
+
+        slice_list.append(row["mistral-7b-instructAI_Slice"])
+        slice_list.append(row["mixstral-8x7b-instructAI_Slice"])
+        slice_list.append(row["llama2-instructAI_Slice"])
+
+        suv_list.append(row["mistral-7b-instructAI_SUV"])
+        suv_list.append(row["mixstral-8x7b-instructAI_SUV"])
+        suv_list.append(row["llama2-instructAI_SUV"])
+
+        slice_count = Coutner(slice_list)
+        suv_count = Counter(suv_list)
+
+        print(slice_count)
+        print(suv_count)
 
         agree_slice = row["num_agree_slice"]
         agree_suv = row["num_agree_suv"]
--- data_prepocessing/data_pipeline.py
+++ data_prepocessing/data_pipeline.py
@@ -2,12 +2,14 @@
 from data_prepocessing.split_sentences import split_sentences
 from data_prepocessing.llm_slice_suv_extraction import llm_slice_suv_extraction
 from data_prepocessing.concenus_voting import concenus_voting
+import pandas as pd
 def run_data_pipeline():
 
-    df = split_sentences()
+    #df = split_sentences()
     #print(df)
     #print(fail)
-    df = llm_slice_suv_extraction(df)
-    print(df)
-    df.to_excel("test.xlsx", index=False, sheet_name='Predictions')
+    #df = llm_slice_suv_extraction(df)
+    #print(df)
+    #df.to_excel("test.xlsx", index=False, sheet_name='Predictions')
+    df = pd.read_excel("test.xlsx")
     concenus_voting(df)
