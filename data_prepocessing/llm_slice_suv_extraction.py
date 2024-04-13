c04d4eec62e702cc5e766649ea8a8a8f64db0364
zhuemann <56049645+zhuemann@users.noreply.github.com>
2024-04-13 15:03:59 -0500

    testing pipeline with llms extraction

--- data_prepocessing/llm_slice_suv_extraction.py
+++ data_prepocessing/llm_slice_suv_extraction.py
@@ -115,7 +115,7 @@ SUV: 3.0
 You are a helpful assistant tasked with extracting values from a sentence. You should return the slice number and its associated suv value for the current scan.
 Sentence: """
 
-    models = ['llama-13b-instruct', 'mistral-7b-instruct', 'mixstral-8x7b-instruct']
+    models = ['mistral-7b-instruct', 'mixstral-8x7b-instruct']
     for model in models:
         ai_slice = []
         ai_suv = []
