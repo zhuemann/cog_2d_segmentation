4d54e6f9dd5b7632d6a79389a1f8e38e7cba3325
zhuemann <56049645+zhuemann@users.noreply.github.com>
2024-04-16 15:52:27 -0500

    testing pipeline with llms extraction

--- data_prepocessing/make_labels_from_point.py
+++ data_prepocessing/make_labels_from_point.py
@@ -181,7 +181,8 @@ def itm(start_point, suv_max, img, conversion, exit_early):
     # get new adjacent pixels above threshold
     #adjacent_pixels = extend_pixels({start_point}, 1)
     #adjacent_pixels = extend_pixels_6_neighbors({start_point}, 1)
-    adjacent_pixels = extend_pixels_21_neighbors({start_point})
+    #adjacent_pixels = extend_pixels_21_neighbors({start_point})
+    adjacent_pixels = extend_pixels({start_point}, 1)
 
     # print(adjacent_pixels)
     new_contour = contour_above_threshold(img, new_threshold, adjacent_pixels)
@@ -205,7 +206,8 @@ def itm(start_point, suv_max, img, conversion, exit_early):
         if new_threshold / source > .8:
             new_threshold = .7 * source
             #canidate_pixels = extend_pixels_6_neighbors(new_contour, 1)
-            canidate_pixels = extend_pixels_21_neighbors(new_contour)
+            #canidate_pixels = extend_pixels_21_neighbors(new_contour)
+            canidate_pixels = extend_pixels(new_contour, 1)
             new_contour = contour_above_threshold(img, new_threshold, canidate_pixels)
             exit_early += 1
             print(f"exited early: {exit_early}")
@@ -216,9 +218,9 @@ def itm(start_point, suv_max, img, conversion, exit_early):
         # print(f"threshold: {new_threshold}")
         # add all adjacent pixels above threshold to the contour
         # get new adjacent pixels above threshold
-        # canidate_pixels = extend_pixels(new_contour, 1)
+        canidate_pixels = extend_pixels(new_contour, 1)
         #canidate_pixels = extend_pixels_6_neighbors(new_contour, 1)
-        canidate_pixels = extend_pixels_21_neighbors(new_contour)
+        #canidate_pixels = extend_pixels_21_neighbors(new_contour)
         new_contour = contour_above_threshold(img, new_threshold, canidate_pixels)
 
         # if new threhold is different by more than 5 percent then continue process else break and return countour
