0082965467dd2ae5e9b0b201b1438d92dba2224d
zhuemann <56049645+zhuemann@users.noreply.github.com>
2024-04-20 20:13:05 -0500

    running pipe to create labels 11 with suv max thresholding and train v23

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ from data_prepocessing.data_pipeline import run_data_pipeline
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
 
-    #run_data_pipeline()
+    run_data_pipeline()
     #print(fail)
     #make_connected_component_labels_for_all_subregions()
     #make_mips_from_3d_data()
