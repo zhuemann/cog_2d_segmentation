be776da3de213c0678a732d3c357c223bcad0a4f
zhuemann <56049645+zhuemann@users.noreply.github.com>
2024-04-23 12:39:37 -0500

    trying updated itm method

--- data_prepocessing/make_labels_from_point.py
+++ data_prepocessing/make_labels_from_point.py
@@ -181,8 +181,8 @@ def itm(start_point, suv_max, img, conversion, exit_early):
 
     # get new adjacent pixels above threshold
     #adjacent_pixels = extend_pixels({start_point}, 1)
-    #adjacent_pixels = extend_pixels_6_neighbors({start_point}, 1)
-    adjacent_pixels = extend_pixels_21_neighbors({start_point})
+    adjacent_pixels = extend_pixels_6_neighbors({start_point}, 1)
+    #adjacent_pixels = extend_pixels_21_neighbors({start_point})
     #adjacent_pixels = extend_pixels({start_point}, 1)
     new_contour = contour_above_threshold(img, new_threshold, adjacent_pixels, set([]))
     #adjacent_pixels = extend_pixels(new_contour, 1)
@@ -204,7 +204,7 @@ def itm(start_point, suv_max, img, conversion, exit_early):
         old_threshold = new_threshold
         new_threshold = calculate_threshold(volume, background, source) * source
         # maybe try something like this
-        if new_threshold / source > .7:
+        if new_threshold / source > .9:
             new_threshold = .4 * source
             #canidate_pixels = extend_pixels_6_neighbors(new_contour, 1)
             #canidate_pixels = extend_pixels_21_neighbors(new_contour)
@@ -221,8 +221,8 @@ def itm(start_point, suv_max, img, conversion, exit_early):
         # add all adjacent pixels above threshold to the contour
         # get new adjacent pixels above threshold
         #canidate_pixels = extend_pixels(new_contour, 1)
-        #canidate_pixels = extend_pixels_6_neighbors(new_contour, 1)
-        canidate_pixels = extend_pixels_21_neighbors(new_contour)
+        canidate_pixels = extend_pixels_6_neighbors(new_contour, 1)
+        #canidate_pixels = extend_pixels_21_neighbors(new_contour)
         new_contour = contour_above_threshold(img, new_threshold, canidate_pixels, new_contour)
 
         # if new threhold is different by more than 5 percent then continue process else break and return countour
