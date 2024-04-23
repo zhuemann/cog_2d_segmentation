a375d6a881a2aaef5daf4505bcc4e3dbc8068cbf
zhuemann <56049645+zhuemann@users.noreply.github.com>
2024-04-23 16:51:29 -0500

    trying updated itm method

--- data_prepocessing/make_labels_from_point.py
+++ data_prepocessing/make_labels_from_point.py
@@ -201,6 +201,7 @@ def itm(start_point, suv_max, img, conversion, exit_early):
             print(f"old background: {background}")
             background = get_background_value(extension=4, pixel_set=new_contour, img=img)
             print(f"new background: {background}")
+            print(new_contour)
         #print(f"background value: {background}")
 
         # calculate volume and background
