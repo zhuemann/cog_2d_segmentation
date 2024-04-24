e3822e5f9d278b5d56fa33d7e26cee0cfb0a5303
zhuemann <56049645+zhuemann@users.noreply.github.com>
2024-04-24 01:14:49 -0500

    making mips for v13 to train overnight

--- data_prepocessing/data_pipeline.py
+++ data_prepocessing/data_pipeline.py
@@ -12,7 +12,7 @@ import pandas as pd
 def run_data_pipeline():
 
     #save_base = "/UserData/Zach_Analysis/suv_slice_text/uw_lymphoma_preprocess_chain_v3/"
-    save_base = "/UserData/Zach_Analysis/suv_slice_text/uw_lymphoma_preprocess_chain_v13/"
+    save_base = "/UserData/Zach_Analysis/suv_slice_text/uw_lymphoma_preprocess_chain_v11/"
 
     save_base_final = "/UserData/Zach_Analysis/petlymph_image_data/"
     """
