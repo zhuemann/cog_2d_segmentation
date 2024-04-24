6e5f0ac6f67e9ee44718894fc2f8cf05d2f664ef
zhuemann <56049645+zhuemann@users.noreply.github.com>
2024-04-23 23:57:24 -0500

    starting the processing chain v14 rerun from the start

--- data_prepocessing/remove_dups_non_anontomical_sent.py
+++ data_prepocessing/remove_dups_non_anontomical_sent.py
@@ -131,21 +131,22 @@ if __name__ == '__main__':
             anatomy_available.append(0)
         else:
             anatomy_available.append(1)
-        """
+
         previous = df['Previous Sentence'][ii]
         annotation, anatomy = find_anatomical_entities(previous, f1radgraph)
         if len(anatomy) == 0:
             anatomy_available_previous.append(0)
         else:
             anatomy_available_previous.append(1)
-
+        print("after first call")
         next = df['Following Sentence'][ii]
         annotation, anatomy = find_anatomical_entities(next, f1radgraph)
         if len(anatomy) == 0:
             anatomy_available_next.append(0)
         else:
             anatomy_available_next.append(1)
-        """
+        print("after second call")
+
 
     df['annotation'] = annotation_list
     df['anatomy'] = anatomy_list
