339ee5e441fbf972c13cc1f8e205c01e13db816a
zhuemann <56049645+zhuemann@users.noreply.github.com>
2024-05-07 10:48:27 -0500

    testing suv conversion for full pet dataset

--- uw_pet_suv_conversion.py
+++ uw_pet_suv_conversion.py
@@ -180,12 +180,16 @@ def convert_PT_CT_files_to_nifti(top_dicom_folder, top_nifti_folder):
     elif dicom_modality == 'PT':
         #print(f"dicom_modality: {dicom_modality}")
         #print(f"about to call dicom to nifiti")
-        dicom2nifti.dicom_series_to_nifti(top_dicom_folder,
+        try:
+            dicom2nifti.dicom_series_to_nifti(top_dicom_folder,
                                           os.path.join(subject_save_folder, scan_save_name + '.nii.gz'),
                                           reorient_nifti=False)
         #print(f" about to call convert to nifiti to suv nifiti")
-        convert_pet_nifti_to_suv_nifti(os.path.join(subject_save_folder, scan_save_name + '.nii.gz'), test_dicom,
+            convert_pet_nifti_to_suv_nifti(os.path.join(subject_save_folder, scan_save_name + '.nii.gz'), test_dicom,
                                        os.path.join(subject_save_folder, scan_save_name + '_SUV.nii.gz'))
+        except ValueError:
+            print("slice error")
+            os.rmdir(subject_save_folder)
 
 
 
@@ -445,7 +449,6 @@ def uw_pet_suv_conversion():
                 print("missing inject time: {e}")
                 missing_inject_info += 1
                 continue
-
             found_pet_images += 1
             continue
         elif any("wb_ac_3d" in element.lower() for element in test):
