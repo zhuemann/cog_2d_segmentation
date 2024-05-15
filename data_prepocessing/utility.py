ad619d5a1f9bc0783316d13152eee81638490358
zhuemann <56049645+zhuemann@users.noreply.github.com>
2024-05-14 21:19:06 -0500

    Counting left and right cases for analysis

--- data_prepocessing/utility.py
+++ data_prepocessing/utility.py
@@ -61,7 +61,7 @@ def count_left_right_sided(df):
 
         sentence = row["sentence"]
 
-        label_name = row["Label_Name"] + ".png"
+        label_name = row["Label_Name"] + ".nii.gz"
         label_path = os.path.join(label_path_base, label_name)
         nii_label = nib.load(label_path)
         label = nii_label.get_fdata()
