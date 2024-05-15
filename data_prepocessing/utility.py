3377a2b9ab3102d66c84383585039ee3d6b3baac
zhuemann <56049645+zhuemann@users.noreply.github.com>
2024-05-15 14:55:19 -0500

    finding missing images in data transfer to pures

--- data_prepocessing/utility.py
+++ data_prepocessing/utility.py
@@ -272,6 +272,8 @@ def analyze_matching_ct_series_for_pt_substring(root_dir, pt_substring):
     for patient_coding in os.listdir(root_dir):
         print(f"index: {index}")
         index += 1
+        if index == 100:
+            break
         patient_path = os.path.join(root_dir, patient_coding)
         if os.path.exists(patient_path) and os.path.isdir(patient_path):  # Check if it's a directory
             # Traverse the directory structure date->modality->exam_name->series
