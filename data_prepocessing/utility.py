00abd15e7f8c906e5f46de66750c5157e0a8b74f
zhuemann <56049645+zhuemann@users.noreply.github.com>
2024-05-15 14:41:44 -0500

    finding missing images in data transfer to pures

--- data_prepocessing/utility.py
+++ data_prepocessing/utility.py
@@ -343,21 +343,21 @@ def analyze_ct_series_when_pt_matches(root_dir, pt_substring):
                                     for series_folder, paths in exam_paths.items():
                                         pt_path = paths.get('PT')
                                         ct_path = paths.get('CT')
-                                        if pt_path and ct_path:  # Both PT and CT exist for the exam
-                                            pt_series_found = False
-                                            # Check PT folders for the specific substring
+                                        #if pt_path and ct_path:  # Both PT and CT exist for the exam
+                                        pt_series_found = False
+                                        # Check PT folders for the specific substring
 
-                                            for pt_series_folder in os.listdir(pt_path):
-                                                print(pt_series_folder)
-                                                if pt_substring.lower() in pt_series_folder.lower():
-                                                    pt_series_found = True
-                                                    print(f"Match found in PT: {pt_series_folder} in {pt_path}")
-                                                    break
-                                            # If PT with specific substring found, count all CT series
-                                            if pt_series_found:
-                                                for ct_series_folder in os.listdir(ct_path):
-                                                    ct_series_count[ct_series_folder] += 1
-                                                    print(f"Counting CT series: {ct_series_folder} in {ct_path}")
+                                        for pt_series_folder in os.listdir(pt_path):
+                                            print(pt_series_folder)
+                                            if pt_substring.lower() in pt_series_folder.lower():
+                                                pt_series_found = True
+                                                print(f"Match found in PT: {pt_series_folder} in {pt_path}")
+                                                break
+                                        # If PT with specific substring found, count all CT series
+                                        if pt_series_found:
+                                            for ct_series_folder in os.listdir(ct_path):
+                                                ct_series_count[ct_series_folder] += 1
+                                                print(f"Counting CT series: {ct_series_folder} in {ct_path}")
 
     if not ct_series_count:  # Check if the dictionary is still empty
         print("No matching PT series found, or no CT series to count.")
