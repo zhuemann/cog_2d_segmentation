f7623a9703974113d8d202caa3aacbfa0c53111b
zhuemann <56049645+zhuemann@users.noreply.github.com>
2024-05-16 18:38:18 -0500

    Running v7, longer at 500 ep

--- main.py
+++ main.py
@@ -60,7 +60,7 @@ if __name__ == '__main__':
     else:
         directory_base = "/UserData/"
 
-    config = {"seed": 1, "batch_size": 32, "dir_base": directory_base, "epochs": 200, "n_classes": 2, "LR": 1e-3,
+    config = {"seed": 1, "batch_size": 32, "dir_base": directory_base, "epochs": 500, "n_classes": 2, "LR": 1e-3,
               "IMG_SIZE": (192, 384), "train_samples": .8, "test_samples": .5, "data_path": "D:/candid_ptx/", #"IMG_SIZE": (128, 256)
               "report_gen": False, "mlm_pretraining": False, "contrastive_training": False, "save_location": ""}
 
@@ -80,7 +80,7 @@ if __name__ == '__main__':
 
         #folder_name = "cropped_mips/baseline_nnunet_v17/seed" + str(seed) + "/"
         #save_string = "/UserData/Zach_Analysis/result_logs/cog_mip_segmentation/initial_testing/" + folder_name
-        folder_name = "/contextual_net_1000ep_uw_dataset_larger_left_right_removed_template_removal_v6/seed" + str(seed) + "/"
+        folder_name = "/contextual_net_1000ep_uw_dataset_larger_left_right_removed_template_removal_longer_v7/seed" + str(seed) + "/"
         save_string = "/UserData/Zach_Analysis/result_logs/visual_grounding/using_mips/initial_testing" + folder_name
         save_location = os.path.join(directory_base, save_string)
         # save_location = ""
