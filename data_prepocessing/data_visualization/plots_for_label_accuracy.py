9259aab7d0598c99c02cb4d6eb17b2367fa87ec6
zhuemann <56049645+zhuemann@users.noreply.github.com>
2024-05-21 01:34:16 -0500

    Working on getting mips for accuracy assessment

--- data_prepocessing/data_visualization/plots_for_label_accuracy.py
+++ data_prepocessing/data_visualization/plots_for_label_accuracy.py
@@ -138,8 +138,11 @@ def plot_for_label_accuracy_assessment(df):
         nii_label = nib.load(label_path)
         label = nii_label.get_fdata()
 
-        ct_label = resample_image(label, ct_volume.shape)
-        nib.save(ct_label,
+        if os.path.exists('/mnt/Bradshaw/UW_PET_Data/raw_nifti_uw_pet/resampled_labels/' + row["Label_Name"] + '.nii.gz'):
+            ct_label = nib.load('/mnt/Bradshaw/UW_PET_Data/raw_nifti_uw_pet/resampled_labels/' + row["Label_Name"] + '.nii.gz')
+        else:
+            ct_label = resample_image(label, ct_volume.shape)
+            nib.save(ct_label,
                  '/mnt/Bradshaw/UW_PET_Data/raw_nifti_uw_pet/resampled_labels/' + row["Label_Name"] + '.nii.gz')
         ct_label = np.round(ct_label).astype(int)
         print(f"ct label dimensions: {ct_label.shape} sum: {np.sum(ct_label)}")
