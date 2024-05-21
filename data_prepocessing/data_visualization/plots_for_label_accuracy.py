1f5630eb7fb55c9b39e15adebe479ac33181ada3
zhuemann <56049645+zhuemann@users.noreply.github.com>
2024-05-21 14:55:24 -0500

    Working on getting mips for accuracy assessment

--- data_prepocessing/data_visualization/plots_for_label_accuracy.py
+++ data_prepocessing/data_visualization/plots_for_label_accuracy.py
@@ -263,7 +263,7 @@ def plot_for_label_accuracy_assessment(df):
         """
         sentence = row["sentence"]
         sentence = insert_newlines(sentence, word_limit=25)
-        plt.suptitle(sentence + " pixels: " + str(np.sum(label_coronal)), fontsize=12, color='black')
+        plt.suptitle(sentence + " Pixels: " + str(np.sum(label_coronal)) +" Slice:" + slice_num + "SUV: " + str(row["SUV"]), fontsize=12, color='black')
 
         plt.savefig("/UserData/Zach_Analysis/petlymph_image_data/prediction_mips_for_presentations/mips_accuracy_assessment/" + label_name)
 
