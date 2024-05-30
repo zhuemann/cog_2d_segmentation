282ced34a019a14a8eaa34b40ed36cfd9ef446c4
zhuemann <56049645+zhuemann@users.noreply.github.com>
2024-05-29 21:21:06 -0500

    Starting processing of uw_all_pet_preprocess_chain_v2 running radgraph over sentences

--- data_prepocessing/remove_non_anontomical_sent.py
+++ data_prepocessing/remove_non_anontomical_sent.py
@@ -58,14 +58,14 @@ def remove_non_anatomical_sent_v2(df):
     # Merge the two DataFrames on 'Petlymph' from both DataFrames and
     # 'sentence' from df with 'Extracted Sentences' from df_anatomical_info
     #merged_df = pd.merge(df, df_anotomical_info, left_on='sentence', right_on='Extracted Sentences')
-    merged_df = pd.merge(df, df_anotomical_info, left_on='Extracted Sentences', right_on='Extracted Sentences')
+    merged_df = pd.merge(df, df_anotomical_info)#, left_on='Extracted Sentences', right_on='Extracted Sentences')
 
     # Filter out rows where 'anatomy_available' is not 0
     filtered_df = merged_df[merged_df['anatomy_available'] != 0]
     print(filtered_df.columns)
     df_dropped = filtered_df.drop(
         columns=['Petlymph_y', 'Findings_y', 'Impression_y', 'Slice_y', 'SUV_y',
-                 'annotation', 'anatomy', 'anatomy_available'])
+                 'annotation', 'anatomy', 'anatomy_available', 'Unnamed: 0_y', 'Unnamed: 0_x'])
 
     df_dropped.rename(columns={'Slice_x': 'Slice'}, inplace=True)
     df_dropped.rename(columns={'SUV_x': 'SUV'}, inplace=True)
@@ -75,6 +75,7 @@ def remove_non_anatomical_sent_v2(df):
     #df_dropped.rename(columns={'Extracted Sentences': 'sentence'}, inplace=True)
 
     print(f"final df length: {len(df_dropped)}")
+    print(filtered_df.columns)
     return df_dropped
 
 def remove_non_anontomical_sent(df):
