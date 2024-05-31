9770395b0b6a15195d17cd474a69bbbefe6cdfec
zhuemann <56049645+zhuemann@users.noreply.github.com>
2024-05-31 11:30:01 -0500

    Tweaking prompt for suv slice extraction

--- main.py
+++ main.py
@@ -53,6 +53,7 @@ if __name__ == '__main__':
     #print(fail)
 
     run_data_pipeline_final()
+
     print(fail)
     #make_connected_component_labels_for_all_subregions()
     #make_mips_from_3d_data()
