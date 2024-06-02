eb36e3bd2fd63f9bd542e57d670d525a613e655d
zhuemann <56049645+zhuemann@users.noreply.github.com>
2024-06-01 23:35:50 -0500

    Trying to detect number of measurments in sentence

--- data_prepocessing/data_pipeline.py
+++ data_prepocessing/data_pipeline.py
@@ -182,21 +182,20 @@ def run_data_pipeline_final():
     df = remove_non_anatomical_sent_v2(df)
     df.to_excel(save_base + "remove_non_anotomical_info_3.xlsx", index=False)
 
-    df = pd.read_excel("/UserData/Zach_Analysis/suv_slice_text/uw_all_pet_preprocess_chain_v3/remove_non_anotomical_info_3.xlsx")
-    df = llm_remove_multiple_descriptions(df)
-    df.to_excel(save_base + "llm_remove_multipled_descriptions_test_5_1000_test_v5.xlsx", index=False, sheet_name='Predictions')
+    #df = pd.read_excel("/UserData/Zach_Analysis/suv_slice_text/uw_all_pet_preprocess_chain_v3/remove_non_anotomical_info_3.xlsx")
+    #df = llm_remove_multiple_descriptions(df)
+    #df.to_excel(save_base + "llm_remove_multipled_descriptions_test_5_1000_test_v5.xlsx", index=False, sheet_name='Predictions')
     #df = pd.read_excel(save_base + "remove_non_anotomical_info_3_try3.xlsx")
-    #df = llm_slice_suv_extraction(df)
-    #df.to_excel(save_base + "model_predictions_for_suv_slice_extraction_4_250_test.xlsx", index=False, sheet_name='Predictions')
+    df = llm_slice_suv_extraction(df)
+    df.to_excel(save_base + "model_predictions_for_suv_slice_extraction_4_test.xlsx", index=False, sheet_name='Predictions')
 
     #df = llm_remove_multiple_descriptions(df)
     #df.to_excel(save_base + "llm_remove_multipled_descriptions_test_5_250_test.xlsx", index=False, sheet_name='Predictions')
 
     # df = pd.read_excel(save_base + "model_predictions_for_suv_slice_extraction_2.xlsx")
-    #df = concenus_voting(df)
-    #df.to_excel(save_base + "concenus_output_5_250_test.xlsx", index=False)
+    df = concenus_voting(df)
+    df.to_excel(save_base + "concenus_output_5.xlsx", index=False)
 
-    """
     df = threshold_suv_max(df)
     df.to_excel(save_base + "thresholded_concensus_outputs_6.xlsx")
 
@@ -205,5 +204,5 @@ def run_data_pipeline_final():
 
     df = get_max_pixel_step3(df)
     df.to_excel(save_base + "max_pixel_9.xlsx", index=False)
-    """
+
 
