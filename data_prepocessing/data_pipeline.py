3fdff64eca6d6e94361754e40cc919af0e73f822
zhuemann <56049645+zhuemann@users.noreply.github.com>
2024-06-01 20:47:05 -0500

    Trying to detect number of measurments in sentence

--- data_prepocessing/data_pipeline.py
+++ data_prepocessing/data_pipeline.py
@@ -165,7 +165,7 @@ def run_data_pipeline_final():
     save_base = "/UserData/Zach_Analysis/suv_slice_text/uw_all_pet_preprocess_chain_v3/"
     save_base_final = "/UserData/Zach_Analysis/petlymph_image_data/"
 
-
+    """
     df_path = "/UserData/Zach_Analysis/lymphoma_data/all_pet_reports_uw.xlsx"
     df = pd.read_excel(df_path)
     df.to_excel(save_base + "initial_data_0.xlsx", index=False)
@@ -181,7 +181,8 @@ def run_data_pipeline_final():
     #df = pd.read_excel(save_base + "remove_multiple_suv_and_slice_2.xlsx")
     df = remove_non_anatomical_sent_v2(df)
     df.to_excel(save_base + "remove_non_anotomical_info_3.xlsx", index=False)
-
+    """
+    df = pd.read_excel("/UserData/Zach_Analysis/lymphoma_data/remove_non_anotomical_info_3.xlsx")
     df = llm_remove_multiple_descriptions(df)
     df.to_excel(save_base + "llm_remove_multipled_descriptions_test_5_250_test.xlsx", index=False, sheet_name='Predictions')
     #df = pd.read_excel(save_base + "remove_non_anotomical_info_3_try3.xlsx")
