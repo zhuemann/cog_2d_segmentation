e3a78c33424ca3a9b8e65b59bd6b550c9e6b0003
zhuemann <56049645+zhuemann@users.noreply.github.com>
2024-06-02 21:57:55 -0500

    Checking why llm extration got miss-matched

--- data_prepocessing/data_pipeline.py
+++ data_prepocessing/data_pipeline.py
@@ -165,7 +165,7 @@ def run_data_pipeline_final():
     save_base = "/UserData/Zach_Analysis/suv_slice_text/uw_all_pet_preprocess_chain_v4/"
     save_base_final = "/UserData/Zach_Analysis/petlymph_image_data/"
 
-
+    """
     df_path = "/UserData/Zach_Analysis/lymphoma_data/all_pet_reports_uw.xlsx"
     df = pd.read_excel(df_path)
     df.to_excel(save_base + "initial_data_0.xlsx", index=False)
@@ -188,10 +188,10 @@ def run_data_pipeline_final():
     #df = pd.read_excel(save_base + "remove_non_anotomical_info_3_try3.xlsx")
     df = llm_slice_suv_extraction(df)
     df.to_excel(save_base + "model_predictions_for_suv_slice_extraction_4_1000_test.xlsx", index=False, sheet_name='Predictions')
-
+    """
     #df = llm_remove_multiple_descriptions(df)
     #df.to_excel(save_base + "llm_remove_multipled_descriptions_test_5_250_test.xlsx", index=False, sheet_name='Predictions')
-
+    df = pd.read_excel(save_base + "model_predictions_for_suv_slice_extraction_4_1000_test.xlsx")
     # df = pd.read_excel(save_base + "model_predictions_for_suv_slice_extraction_2.xlsx")
     df = concenus_voting(df)
     df.to_excel(save_base + "concenus_output_5.xlsx", index=False)
