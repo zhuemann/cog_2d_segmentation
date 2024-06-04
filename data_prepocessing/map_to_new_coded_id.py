0ece405b15deef958b5216983aeb3fa909ec3e46
zhuemann <56049645+zhuemann@users.noreply.github.com>
2024-06-03 23:34:30 -0500

    Running max pixel on first 10000

--- data_prepocessing/map_to_new_coded_id.py
+++ data_prepocessing/map_to_new_coded_id.py
@@ -6,8 +6,8 @@ def map_to_new_coded_id(df):
     df_coded = pd.read_excel("/UserData/UW_PET_Data/UWPETCTWB_Research-Image-Requests_20240311.xlsx")
     print(df_coded.columns)
     # Create a mapping from Original to Coded Accession Numbers
-    mapping = df_coded.set_index('Original Accession Number')['Coded Accession Number']
+    mapping = df_coded.set_index('Original Accession Number')['Coded Accession Number'].to_dict()
     # Replace the Original Accession Numbers with Coded Accession Numbers in df_original
-    df['Petlymph'] = df['Original Accession Number'].map(mapping)
+    df['Petlymph'] = df['Petlymph'].map(mapping)
 
     return df
