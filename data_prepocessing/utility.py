a336481882e5c27ad41ddf6e1d69149d3962a79e
zhuemann <56049645+zhuemann@users.noreply.github.com>
2024-06-10 16:15:46 -0500

    Making labels for uw_labels_v4_nifti

--- data_prepocessing/utility.py
+++ data_prepocessing/utility.py
@@ -104,7 +104,7 @@ def count_left_right_sided(df, label_path_base):
     #labels_to_skip = ["PETWB_006370_04_label_2", "PETWB_011355_01_label_5", "PETWB_002466_01_label_1",
     #                  "PETWB_012579_01_label_2", "PETWB_003190_01_label_3",
     #                  "PETWB_011401_02_label_3"]
-    labels_to_skip = ["PETWB_011355_01_label_5.nii.gz"]
+    labels_to_skip = ["PETWB_011355_01_label_5"]
     left_indices = []
     right_indices = []
     left_and_right_count = 0
