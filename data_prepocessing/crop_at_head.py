a443c8d6ebe0d71041b41895ac0aca03a749df99
zhuemann <56049645+zhuemann@users.noreply.github.com>
2024-06-12 16:23:22 -0500

    Using pet to do head cropping if there is pet availbe else use ct and create the master document

--- data_prepocessing/crop_at_head.py
+++ data_prepocessing/crop_at_head.py
@@ -80,7 +80,9 @@ def crop_at_head_calculation(df):
         #    continue
         #if index > 1100 and index < 7000:
         #    continue
-        if index > 1000:
+        if index < 10000:
+            continue
+        if index > 11000:
             break
 
         #if images_created == 500:
@@ -182,7 +184,7 @@ def crop_at_head_calculation(df):
             axes[1].axhline(y=z_plane, color='r', linestyle='--', label=f'z-plane {z_plane}')
 
             # Plot the CT projection on axis 0
-            axes[2].imshow(ct_max_projection_2d_axis0.T, cmap='bone', origin='lower', vmax=10, vmin=0)
+            axes[2].imshow(ct_max_projection_2d_axis0.T, cmap='gray', origin='lower') #, vmax=10, vmin=0)
             axes[2].set_title(f'CT Maximum Intensity Projection (Axis 0) max: {np.max(ct_max_projection_2d_axis0)}')
             axes[2].set_xlabel('Y-axis')
             axes[2].set_ylabel('Z-axis')
