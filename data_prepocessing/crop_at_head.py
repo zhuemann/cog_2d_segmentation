843a550a94157d8039d159872d87ea06e04232b4
zhuemann <56049645+zhuemann@users.noreply.github.com>
2024-06-12 12:04:03 -0500

    testing with llama3 instruct to find multiple descirptions

--- data_prepocessing/crop_at_head.py
+++ data_prepocessing/crop_at_head.py
@@ -58,7 +58,7 @@ def crop_at_head_calculation(df):
         print(f"folder: {folder}")
         current_path = os.path.join(base_folder, folder)
 
-        suv_dicom = master_dicom_location[master_dicom_location['Petlymph'] == folder]["PT_Path"]
+        suv_dicom = master_dicom_location[master_dicom_location['Petlymph'] == folder] #["PT_Path"]
         print(suv_dicom)
 
         if True:
