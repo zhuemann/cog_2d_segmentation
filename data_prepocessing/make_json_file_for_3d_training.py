d0ea823f0e34d4c3526a4077b832edf8046d997e
zhuemann <56049645+zhuemann@users.noreply.github.com>
2024-06-15 13:27:15 -0500

    Working on plotting of 3d_predictions

--- data_prepocessing/make_json_file_for_3d_training.py
+++ data_prepocessing/make_json_file_for_3d_training.py
@@ -24,9 +24,9 @@ def get_suv_images_list(df):
     df.to_excel("/UserData/Zach_Analysis/suv_slice_text/full_suv_names_uw_pet_4.xlsx", index=False)
     return df
 
-def filter_dataframe_for_images_in_folder(df):
+def filter_dataframe_for_images_in_folder(df, target_folder):
     # Define your target folder
-    target_folder = "/mnt/Bradshaw/UW_PET_Data/resampled_cropped_images_and_labels/images4/"
+    #target_folder = "/mnt/Bradshaw/UW_PET_Data/resampled_cropped_images_and_labels/images4/"
 
     # Get a list of all files in the target folder
     files = os.listdir(target_folder)
@@ -43,8 +43,10 @@ def make_json_file_for_3d_training(df):
     label_path_base = "/mnt/Bradshaw/UW_PET_Data/resampled_cropped_images_and_labels/labels4/"
 
     print(f"length of dataframe before: {len(df)}")
-    df = filter_dataframe_for_images_in_folder(df)
-    print(f"after inital filtering: {len(df)}")
+    df = filter_dataframe_for_images_in_folder(df, target_folder="/mnt/Bradshaw/UW_PET_Data/resampled_cropped_images_and_labels/images4/")
+    print(f"after image filtering: {len(df)}")
+    df = filter_dataframe_for_images_in_folder(df, target_folder="/mnt/Bradshaw/UW_PET_Data/resampled_cropped_images_and_labels/labels4/")
+    print(f"after label filtering: {len(df)}")
     labels_to_skip = ["PETWB_006370_04_label_2", "PETWB_011355_01_label_5", "PETWB_002466_01_label_1",
                       "PETWB_012579_01_label_2", "PETWB_003190_01_label_3", "PETWB_013006_03_label_2"]
     df = df[~df["Label_Name"].isin(labels_to_skip)]
