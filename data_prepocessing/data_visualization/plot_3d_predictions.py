ae638631903f62207f48d5b9cf33ce3789918875
zhuemann <56049645+zhuemann@users.noreply.github.com>
2024-06-15 13:20:22 -0500

    Working on plotting of 3d_predictions

--- data_prepocessing/data_visualization/plot_3d_predictions.py
+++ data_prepocessing/data_visualization/plot_3d_predictions.py
@@ -6,25 +6,23 @@ def plot_3d_predictions():
 
     prediction_location = "/UserData/Zach_Analysis/git_multimodal/3DVision_Language_Segmentation_forked2/COG_dynunet_baseline/COG_dynunet_0_baseline/dynunet_0_0/prediction_trash_v2testing/"
 
-    image_base = "/mnt/Bradshaw/UW_PET_Data/SUV_images/"
+    image_base = "/mnt/Bradshaw/UW_PET_Data/resampled_cropped_images_and_labels/"
     labels_list = os.listdir(prediction_location)
+    all_images = os.listdir(image_base)
 
     for label in labels_list:
 
-        image_name = label[:--7]
-        print(image_name)
-        current_path = os.path.join(image_base, folder)
-
-        ct_path_final = None
-        suv_path_final = None
-
-        for file_name in os.listdir(current_path):
-            if "CT" in file_name:
-                ct_path_final = os.path.join(current_path, file_name)
-            if "SUV" in file_name:
-                suv_path_final = os.path.join(current_path, file_name)
+        image_name = label[:-15]
+        print(f"image name: {image_name}")
+        label_name = label[:-7]
+        print(label_name)
 
 
+        suv_path_final = os.path.join(image_base, image_name + "_suv_cropped.nii.gz")
+        ct_path_final = suv_path_file = os.path.join(image_base, image_name + "_ct_cropped.nii.gz")
+
+        print(suv_path_final)
+        print(ct_path_final)
         full_label_path = os.path.join(prediction_location, label)
         # Load the image file
         nii_image = nib.load(full_label_path)
