b1dc9d2aaff52ae4074e6298f68f766ac37bbfbf
zhuemann <56049645+zhuemann@users.noreply.github.com>
2024-06-26 01:07:25 -0500

    Checking cropping

--- data_prepocessing/data_pipeline.py
+++ data_prepocessing/data_pipeline.py
@@ -233,9 +233,9 @@ def run_data_pipeline_final():
     #df = pd.read_excel(save_base + "removed_wrong_suv_max_and_slices_13.xlsx")
     #make_json_file_for_3d_training(df)
 
-    sampled_df, orig_df = select_250_images_from_json()
-    sampled_df.to_excel(save_base + "for_daniel_250_round_3_final_testset_json.xlsx", index=False)
-    orig_df.to_excel(save_base + "for_daniel_250_round_3_final_testset_standard.xlsx", index=False)
+    #sampled_df, orig_df = select_250_images_from_json()
+    #sampled_df.to_excel(save_base + "for_daniel_250_round_3_final_testset_json.xlsx", index=False)
+    #orig_df.to_excel(save_base + "for_daniel_250_round_3_final_testset_standard.xlsx", index=False)
     #plot_final_testset(sampled_df)
 
     #df = df.drop_duplicates(subset="Petlymph", keep="first")
