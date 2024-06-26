256fca75e4aae38107e7223d606e702f18fc719d
zhuemann <56049645+zhuemann@users.noreply.github.com>
2024-06-25 22:59:29 -0500

    Making plots for initial final testset read

--- data_prepocessing/data_visualization/plot_final_test_set.py
+++ data_prepocessing/data_visualization/plot_final_test_set.py
@@ -248,6 +248,7 @@ def plot_final_testset(df):
         for contour in contours:
             ax5.plot(contour[:, 1], contour[:, 0], linewidth=2, color='red')  # contour[:, 1] is x, contour[:, 0] is y
         """
+        print(original_row)
         sentence = original_row["sentence"][0]
         print(type(sentence))
         sentence = insert_newlines(sentence, word_limit=25)
