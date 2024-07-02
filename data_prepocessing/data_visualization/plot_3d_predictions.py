9b5b4f2f1af2cb808cc4ec19955c0ab7366ec646
zhuemann <56049645+zhuemann@users.noreply.github.com>
2024-07-02 01:53:20 -0500

    plotting 3d high sensitivty mips

--- data_prepocessing/data_visualization/plot_3d_predictions.py
+++ data_prepocessing/data_visualization/plot_3d_predictions.py
@@ -142,14 +142,14 @@ def plot_3d_predictions():
 
         # Plot 2: Prediction MIP overlayed on SUV MIP
         axes[1].imshow(suv_mip.T, cmap='gray_r', aspect='auto', origin='lower', vmin = 0, vmax = 10)
-        axes[1].imshow(prediction_mip.T, alpha=norm(prediction_mip.T), aspect='auto', origin='lower')
+        axes[1].imshow(prediction_mip.T, color="spring" alpha=norm(prediction_mip.T), aspect='auto', origin='lower')
         axes[1].set_title(f'Prediction Overlay on SUV MIP predicted suv_max: {prediction_suv_max}')
         axes[1].axis('off')
 
         # Plot 3: Both Prediction and Label MIP overlayed on SUV MIP
         axes[2].imshow(suv_mip.T, cmap='gray_r', aspect='auto', origin='lower', vmin = 0, vmax = 10)
         axes[2].imshow(label_mip.T, alpha=norm(label_mip.T), aspect='auto', origin='lower')
-        #axes[2].imshow(prediction_mip.T, alpha=norm(prediction_mip.T), aspect='auto', origin='lower')
+        axes[2].imshow(prediction_mip.T, color="spinrg", alpha=norm(prediction_mip.T), aspect='auto', origin='lower')
         axes[2].set_title(f'Prediction and Label Overlay on SUV MIP is correct: {correct}')
         axes[2].axis('off')
 
