3897305b86354086edc3f49b2afa115d68bda780
zhuemann <56049645+zhuemann@users.noreply.github.com>
2024-07-15 17:41:29 -0500

    running post processing for f1=.65

--- data_prepocessing/data_visualization/post_processing_eval.py
+++ data_prepocessing/data_visualization/post_processing_eval.py
@@ -273,7 +273,7 @@ def post_processing_eval():
         prediction_data = np.squeeze(prediction_data, axis=(0, 1))
         #print(f"pred data size: {prediction_data.shape}")
         #prediction_data = analyze_and_filter_volume(prediction_data)
-        prediction_data = filter_prediction_by_average(prediction_data)
+        #prediction_data = filter_prediction_by_average(prediction_data)
         # load in label data
         nii_label = nib.load(label_full_path)
         label_data = nii_label.get_fdata()
