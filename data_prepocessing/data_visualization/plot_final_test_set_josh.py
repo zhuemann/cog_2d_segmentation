f66f8f14d1a8634a8186400c442806eef1d72115
zhuemann <56049645+zhuemann@users.noreply.github.com>
2024-07-21 18:14:11 -0500

    Try plotting final testset for josh but a test for now

--- data_prepocessing/data_visualization/plot_final_test_set_josh.py
+++ data_prepocessing/data_visualization/plot_final_test_set_josh.py
@@ -275,7 +275,7 @@ def plot_final_testset_for_josh_v3(df):
         ax7 = plt.subplot(1, 7, 7)
         ax7.imshow(ct_mip_sagittal, cmap='gray', vmax=600, vmin=-300, aspect=ct_dims[2] / ct_dims[1])
         ax7.imshow(np.where(label_sagittal == 1, 250, np.nan), cmap='spring', alpha=0.9,
-                   aspect=voxel_dims[0] / ct_dims[1])
+                   aspect=ct_dims[0] / voxel_dims[1])
         # plt.imshow(np.where(outline == 1, 250, np.nan) , cmap='spring', alpha=0.9) # Overlay the outline in 'spring' colormap
         ax7.set_title(f'Axial Slice: {slice_num} With Label')
 
