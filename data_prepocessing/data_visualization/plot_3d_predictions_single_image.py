543822839af8edae1b9aa740b3c10f8d1423f3bb
zhuemann <56049645+zhuemann@users.noreply.github.com>
2024-07-22 17:22:52 -0500

    try making single image plots

--- data_prepocessing/data_visualization/plot_3d_predictions_single_image.py
+++ data_prepocessing/data_visualization/plot_3d_predictions_single_image.py
@@ -108,7 +108,7 @@ def plot_all_images():
 
     pet_images = "/mnt/Bradshaw/UW_PET_Data/resampled_cropped_images_and_labels/images5/"
 
-    save_location = "/UserData/Zach_Analysis/petlymph_image_data/prediction_mips_for_presentations/single_plot_predictions/"
+    save_location = "/UserData/Zach_Analysis/petlymph_image_data/prediction_mips_for_presentations/single_plot_predictions_v2/"
 
     df = pd.read_excel("/UserData/Zach_Analysis/suv_slice_text/uw_all_pet_preprocess_chain_v4/removed_wrong_suv_max_and_slices_13.xlsx")
 
