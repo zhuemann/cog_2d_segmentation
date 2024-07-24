79d7abac28b4d482d9975c8669f5dfabdc91faa6
zhuemann <56049645+zhuemann@users.noreply.github.com>
2024-07-24 00:12:14 -0500

    Trying to run external ct conversion

--- main.py
+++ main.py
@@ -29,6 +29,7 @@ from data_prepocessing.utility.utility import generate_data_sheet_on_uw_pet_data
 from uw_pet_suv_conversion import uw_pet_suv_conversion_v2
 from uw_pet_suv_conversion import uw_ct_suv_conversion_v2
 from uw_pet_suv_conversion import file_exploration_analysis_pet
+from uw_pet_suv_conversion import uw_ct_conversion_external_dataset_v2
 from data_prepocessing.data_pipeline import run_data_pipeline_final
 from data_prepocessing.utility.utility import count_files_in_suv_folder
 from data_prepocessing.data_visualization.plot_training_and_inference_images import plot_training_and_inference_images
@@ -47,7 +48,8 @@ if __name__ == '__main__':
     #uw_pet_suv_conversion()
     #uw_pet_suv_conversion_v2()
     #uw_ct_suv_conversion_v2()
-    file_exploration_analysis_pet()
+    #file_exploration_analysis_pet()
+    uw_ct_conversion_external_dataset_v2()
     print(fail)
     #ct_series_count = analyze_ct_series_when_pt_matches(root_dir="/mnt/Bradshaw/UW_PET_Data/dsb2b/",  pt_substring="WB_IRCTAC")
     #print(ct_series_count)
