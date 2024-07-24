a48f58080aaca8e5df114cb57d8e778f884d553f
zhuemann <56049645+zhuemann@users.noreply.github.com>
2024-07-23 23:53:03 -0500

    run ct check on external dataset

--- main.py
+++ main.py
@@ -34,8 +34,8 @@ from data_prepocessing.data_visualization.plot_training_and_inference_images imp
 from uw_pet_suv_conversion import uw_ct_check
 if __name__ == '__main__':
 
-    uw_ct_check()
-    print(fail)
+    #uw_ct_check()
+    #print(fail)
     #count_files_in_suv_folder()
     #print(fail)
     #generate_data_sheet_on_uw_pet_dataset()
@@ -45,8 +45,8 @@ if __name__ == '__main__':
     #get_suv_file_names(df)
     #uw_pet_suv_conversion()
     #uw_pet_suv_conversion_v2()
-    #uw_ct_suv_conversion_v2()
-    #print(fail)
+    uw_ct_suv_conversion_v2()
+    print(fail)
     #ct_series_count = analyze_ct_series_when_pt_matches(root_dir="/mnt/Bradshaw/UW_PET_Data/dsb2b/",  pt_substring="WB_IRCTAC")
     #print(ct_series_count)
     #ct_series_count = analyze_matching_ct_series_for_pt_substring(root_dir="/mnt/Bradshaw/UW_PET_Data/dsb2b/",  pt_substring="WB_IRCTAC")
