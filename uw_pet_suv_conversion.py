91f050f58b925886d7e4f4a70cac4541884cfa4a
zhuemann <56049645+zhuemann@users.noreply.github.com>
2024-07-24 21:38:43 -0500

    converting pets of external testset

--- uw_pet_suv_conversion.py
+++ uw_pet_suv_conversion.py
@@ -1424,11 +1424,14 @@ def uw_pet_suv_conversion_external_v3():
                 top_dicom_folder = os.path.join(directory, matched_recon)
                 #top_dicom_folder = directory + "/" + str(matched_recon) + ""
                 #print(f"top dicom folder: {top_dicom_folder}")
+                found_pet_images = call_suv_helper(top_dicom_folder, top_nifti_folder, found_pet_images)
+                """
                 try:
                     found_pet_images = call_suv_helper(top_dicom_folder, top_nifti_folder, found_pet_images)
                 except Exception as e:
                     print(f"error: {e}")
                     continue  # If an error occurs, continue with the next substring
+                """
 
 
 
