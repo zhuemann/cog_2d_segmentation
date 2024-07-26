3ed30ffa4b2df5cf042b15279c77319e8f4b1467
zhuemann <56049645+zhuemann@users.noreply.github.com>
2024-07-26 11:25:54 -0500

    get full pt list

--- external_test_set_creation/get_ct_paths.py
+++ external_test_set_creation/get_ct_paths.py
@@ -70,8 +70,8 @@ def ct_check():
                 num_modality["CT"] += 1
             #else:
             #    continue
-            if "CT" in modality:
-                directory = os.path.join(directory, "CT")
+            if "PT" in modality:
+                directory = os.path.join(directory, "PT")
             #else:
                 #print(f"file: {file} does not have Pet scan")
                 #continue
@@ -116,7 +116,7 @@ def ct_check():
     columns = ['File'] + [f'String {i + 1}' for i in range(max_len - 1)]
     df.columns = columns
     #print(df)
-    path = '/UserData/Zach_Analysis/suv_slice_text/swedish_hospital_external_data_set/external_test_set_ct_full.xlsx'
+    path = '/UserData/Zach_Analysis/suv_slice_text/swedish_hospital_external_data_set/external_test_set_PT_full.xlsx'
     print(path)
     # Save DataFrame to Excel file
     df.to_excel(path, index=False)
