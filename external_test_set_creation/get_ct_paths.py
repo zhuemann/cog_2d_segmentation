797f07fd2b4a65f716b1d125fa8301f297724187
zhuemann <56049645+zhuemann@users.noreply.github.com>
2024-07-26 11:14:10 -0500

    get full ct list

--- external_test_set_creation/get_ct_paths.py
+++ external_test_set_creation/get_ct_paths.py
@@ -111,5 +111,6 @@ def ct_check():
     columns = ['File'] + [f'String {i + 1}' for i in range(max_len - 1)]
     df.columns = columns
     #print(df)
+    path = '/UserData/Zach_Analysis/visual_ground_file_lists/external_testset_ct_full.xlsx'
     # Save DataFrame to Excel file
-    df.to_excel('/UserData/Zach_Analysis/visual_ground_file_lists/external_testset_ct_full.xlsx', index=False)
+    df.to_excel(path, index=False)
