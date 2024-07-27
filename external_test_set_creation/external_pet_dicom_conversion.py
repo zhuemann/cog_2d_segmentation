bc7bdce22a811f15c23d16342d8493debac50f7b
zhuemann <56049645+zhuemann@users.noreply.github.com>
2024-07-26 20:35:46 -0500

    converting pets to nifti

--- external_test_set_creation/external_pet_dicom_conversion.py
+++ external_test_set_creation/external_pet_dicom_conversion.py
@@ -56,20 +56,16 @@ def pet_suv_conversion_external_v3():
             continue
         directory = os.path.join(dir_path, file)
 
-        print(directory)
         random_id = os.listdir(directory)
-        print(f"random id: {random_id}")
         if len(random_id) == 1:
             directory = os.path.join(directory, random_id[0])
 
-        print(directory)
         date = os.listdir(directory)
         if len(date) == 1:
             directory = os.path.join(directory, date[0])
         else:
             print(f"multiple date files in this folder: {directory}")
         modality = os.listdir(directory)
-        print(f"modality: {modality}")
         if "PT" in modality:
             #directory = os.path.join(dir_path, file, "PT")
             directory = os.path.join(directory, "PT")
@@ -107,6 +103,7 @@ def pet_suv_conversion_external_v3():
                 #print(f"top dicom folder: {top_dicom_folder}")
 
                 try:
+                    print("called pet conversion")
                     found_pet_images = call_suv_helper(top_dicom_folder, top_nifti_folder, found_pet_images)
                 except Exception as e:
                     print(f"error: {e}")
