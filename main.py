d47b41579f56ae38bcc043c54dbd525082b2cfff
zhuemann <56049645+zhuemann@users.noreply.github.com>
2024-07-26 23:30:42 -0500

    converting cts to nifti

--- main.py
+++ main.py
@@ -39,12 +39,15 @@ from uw_pet_suv_conversion import uw_ct_check
 
 from external_test_set_creation.get_ct_paths import ct_check
 from external_test_set_creation.external_pet_dicom_conversion import pet_suv_conversion_external_v3
+from external_test_set_creation.external_ct_dicom_conversion import uw_ct_conversion_external_dataset_v2
+
 
 if __name__ == '__main__':
 
     #uw_pet_suv_conversion_external_v3()
     #ct_check()
-    pet_suv_conversion_external_v3()
+    #pet_suv_conversion_external_v3()
+    uw_ct_conversion_external_dataset_v2()
     print(fail)
     #print(fail)
     #uw_ct_check()
