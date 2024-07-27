1b412172e76a2c6250d92da349307c8652d2db47
zhuemann <56049645+zhuemann@users.noreply.github.com>
2024-07-26 20:57:45 -0500

    converting pets to nifti

--- external_test_set_creation/external_pet_dicom_conversion.py
+++ external_test_set_creation/external_pet_dicom_conversion.py
@@ -97,7 +97,7 @@ def pet_suv_conversion_external_v3():
             print(f"matched: {matched_recon}")
             if matched_recon:
                 # If a match is found, build the path
-                top_dicom_folder = os.path.join(directory, matched_recon)
+                top_dicom_folder = os.path.join(directory, matched_recon, file)
                 #top_dicom_folder = directory + "/" + str(matched_recon) + ""
                 #print(f"top dicom folder: {top_dicom_folder}")
 
