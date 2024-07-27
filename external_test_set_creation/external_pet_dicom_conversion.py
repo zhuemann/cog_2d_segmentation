37f37835c355ca16bcbad6f78bca9eadc017185b
zhuemann <56049645+zhuemann@users.noreply.github.com>
2024-07-26 22:15:56 -0500

    converting pets to nifti

--- external_test_set_creation/external_pet_dicom_conversion.py
+++ external_test_set_creation/external_pet_dicom_conversion.py
@@ -92,7 +92,7 @@ def pet_suv_conversion_external_v3():
         for substring in substrings_to_check:
             # Normalize to lower case for case-insensitive comparison
             matched_recon = next((recon for recon in recon_types if substring.lower() in recon.lower()), None)
-
+            print(f"matched recond: {matched_recon}")
             if matched_recon == None or "fused" in matched_recon.lower() or "mip" in matched_recon.lower():
                 continue
             print(f"matched: {matched_recon}")
