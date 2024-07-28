d611446bdabe29a88a40f2b4ff80c5f257e1f5d8
zhuemann <56049645+zhuemann@users.noreply.github.com>
2024-07-28 01:01:50 -0500

    Trying get max pixel on files so far

--- external_test_set_creation/external_get_max_pixel.py
+++ external_test_set_creation/external_get_max_pixel.py
@@ -221,7 +221,12 @@ def get_max_pixel_step3(df):
             slice_tolerance = 0
             suv_tolerance = 0.1
             #suv_tolerance = suv_ref*0.05
-            #slice_ref = img.shape[2] - slice_ref
+            slice_ref = int(row["Slice"]) # if this is pet slice number
+            slice_ref = img.shape[2] - slice_ref
+            # if this is ct slice number
+            ct_from_head = ct_image.shahpe[2] - row["Slice"]
+            pet_from_head = ct_from_head/ct_dimensions[2]*pet_dimensions
+            slice_ref = pet_from_head
             found_items = 0
             for key, value in max_suv_dic.items():
                 suv_max, slice_min, slice_max, pixel = value
