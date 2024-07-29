7f8ceac54092f0efe74e2a64777a3d4b1ded47ad
zhuemann <56049645+zhuemann@users.noreply.github.com>
2024-07-28 19:00:30 -0500

    Try converting fused pet

--- uw_pet_suv_conversion.py
+++ uw_pet_suv_conversion.py
@@ -189,9 +189,9 @@ def convert_PT_CT_files_to_nifti(top_dicom_folder, top_nifti_folder):
     subject_save_folder = os.path.join(top_nifti_folder, subject_save_name)
     print(f"subject save folder: {subject_save_folder}")
     os.makedirs(subject_save_folder, exist_ok=True)
-    #scan_save_name = '{}_{}_{}_{}'.format(subject_save_name, dicom_study_date, dicom_modality, \
-    #                                      dicom_series_description.replace(' ', '_'))
-    scan_save_name = '{}_{}_{}'.format(subject_save_name, dicom_study_date, dicom_modality)
+    scan_save_name = '{}_{}_{}_{}'.format(subject_save_name, dicom_study_date, dicom_modality, \
+                                          dicom_series_description.replace(' ', '_'))
+    #scan_save_name = '{}_{}_{}'.format(subject_save_name, dicom_study_date, dicom_modality)
 
     print(f"save save name: {scan_save_name}")
     if dicom_modality in ['CT', 'MR', 'NM']:
