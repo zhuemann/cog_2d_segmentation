72fb8d3e6459ced21a3d597636756b82b2836666
zhuemann <56049645+zhuemann@users.noreply.github.com>
2024-07-29 19:24:57 -0500

    test eval with tracer

--- data_prepocessing/data_visualization/post_processing_eval.py
+++ data_prepocessing/data_visualization/post_processing_eval.py
@@ -265,7 +265,7 @@ def post_processing_eval():
         petlymph_name = image_name.strip(".nii.gz")
         #print(petlymph_name)
 
-        labeled_row = labeled_subset[labeled_subset["Petlymph"] == petlymph_name]
+        labeled_row = labeled_subset[labeled_subset["Label_Name"] == label_name]
 
 
         # Check if labeled_row is empty or it is a bad label
