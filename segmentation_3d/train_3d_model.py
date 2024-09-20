cfd86fab88c7bcc4dda4163bfcba4bf8a08f9157
zhuemann <56049645+zhuemann@users.noreply.github.com>
2024-09-20 13:57:11 -0500

    Starting to build 3d version for github. Adding dataloding using dataframes not json. Saving augmented data as test. Added model and 3d language cross attention

--- segmentation_3d/train_3d_model.py
+++ segmentation_3d/train_3d_model.py
@@ -606,7 +606,7 @@ def train_3d_image_text_segmentation(config, batch_size=8, epoch=1, dir_base = "
             targets = data['targets'].to(device, dtype=torch.float)
             targets = torch.squeeze(targets)
             images = data['images']['pet'].to(device, dtype=torch.float)
-
+            print(f"images: {images.shape}")
             #print(targets.sum())
             #outputs = test_obj(images, ids, mask)  # for lavt
             outputs = test_obj(images, ids, mask, token_type_ids)
