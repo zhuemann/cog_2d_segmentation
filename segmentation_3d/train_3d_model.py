1483746e74b38d08070a666f15ca1e6198760d1b
zhuemann <56049645+zhuemann@users.noreply.github.com>
2024-09-24 15:41:46 -0500

    Starting to build 3d version for github. Adding dataloding using dataframes not json. Saving augmented data as test. Added model and 3d language cross attention

--- segmentation_3d/train_3d_model.py
+++ segmentation_3d/train_3d_model.py
@@ -420,7 +420,8 @@ def train_3d_image_text_segmentation(config, batch_size=8, epoch=1, dir_base = "
         return data_dic
 
     #length = 352
-    length = 176
+    #length = 176
+    length = 88
 
     transforms_resize = Compose([
         #debug_spatial_transform,
