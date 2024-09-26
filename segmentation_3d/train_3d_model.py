10d09b10b3d2a148e5793531846e79d19730d04f
zhuemann <56049645+zhuemann@users.noreply.github.com>
2024-09-26 15:42:29 -0500

    Starting to build 3d version for github. Adding dataloding using dataframes not json. Saving augmented data as test. Added model and 3d language cross attention

--- segmentation_3d/train_3d_model.py
+++ segmentation_3d/train_3d_model.py
@@ -768,7 +768,7 @@ def train_3d_image_text_segmentation(config, batch_size=8, epoch=1, dir_base = "
             del outputs, targets
             #gc.collect()
             #torch.cuda.empty_cache()
-            if index % 5 == 0:
+            if index % 1000 == 0:
                 print(f"index: {index + 1} True positive: {run_tp} False positive: {run_fp} False negative: {run_fn} Running Average Dice: {sum(run_dice) / len(run_dice)}")
 
         avg_training_dice = np.average(training_dice)
