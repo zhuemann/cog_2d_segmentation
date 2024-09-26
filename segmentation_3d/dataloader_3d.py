2bd9d88d22c00aac55217efef28f09859c78f765
zhuemann <56049645+zhuemann@users.noreply.github.com>
2024-09-26 11:15:39 -0500

    Starting to build 3d version for github. Adding dataloding using dataframes not json. Saving augmented data as test. Added model and 3d language cross attention

--- segmentation_3d/dataloader_3d.py
+++ segmentation_3d/dataloader_3d.py
@@ -167,14 +167,14 @@ class TextImageDataset(Dataset):
         # Define an affine transformation matrix (identity matrix by default)
         affine = np.eye(4)
         # Create a NIfTI image from the NumPy array
-        nii_img = nib.Nifti1Image(pet_img, affine)
+        #nii_img = nib.Nifti1Image(pet_img, affine)
 
         # Save the NIfTI image as a .nii.gz file
         #nib.save(nii_img, save_path)
 
         # Create a NIfTI image from the NumPy array
         #nii_img = nib.Nifti1Image(transformed.detach().cpu().numpy(), affine)
-        nii_img = nib.Nifti1Image(pet_img, affine)
+        nii_img = nib.Nifti1Image(pet_img.detach().cpu().numpy(), affine)
         save_path = "/UserData/Zach_Analysis/test_folder/saved_augmented_data/augmented_" + str(index) + ".nii.gz"
         nib.save(nii_img, save_path)
 
