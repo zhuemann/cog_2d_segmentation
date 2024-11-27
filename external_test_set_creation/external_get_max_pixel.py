76a72b0c7a75c97bdbe7c7168b68d3a066a41ba3
zhuemann <56049645+zhuemann@users.noreply.github.com>
2024-11-27 16:38:03 -0600

    testing dynamically switching indexing position

--- external_test_set_creation/external_get_max_pixel.py
+++ external_test_set_creation/external_get_max_pixel.py
@@ -329,5 +329,5 @@ def external_get_max_pixel():
     df = pd.read_excel('/UserData/Zach_Analysis/suv_slice_text/swedish_hospital_external_data_set/swedish_dataframe_test.xlsx')
     df = get_max_pixel_step3(df)
 
-    df.to_excel('/UserData/Zach_Analysis/suv_slice_text/swedish_hospital_external_data_set/swedish_dataframe_max_pixels_v3_index_other_side.xlsx')
+    df.to_excel('/UserData/Zach_Analysis/suv_slice_text/swedish_hospital_external_data_set/swedish_dataframe_max_pixels_v4_orientation_fix.xlsx')
     print(df)
