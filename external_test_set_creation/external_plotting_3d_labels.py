84cd28aede4188f5930495b2434d438d2d2118c5
zhuemann <56049645+zhuemann@users.noreply.github.com>
2024-12-01 23:23:54 -0600

    Try making labels for external set and plotting them

--- external_test_set_creation/external_plotting_3d_labels.py
+++ external_test_set_creation/external_plotting_3d_labels.py
@@ -111,7 +111,7 @@ def plot_external_testset():
 
         ct_volume = rotated_volume
 
-        slice_num = row["slice_num"]
+        slice_num = row["Slice"]
         # k_num = row["k"]
         original_row = original_df.loc[original_df['Label_Name'] == label_name]
         #crop_row = crop_df.loc[crop_df['id'] == petlymph]
