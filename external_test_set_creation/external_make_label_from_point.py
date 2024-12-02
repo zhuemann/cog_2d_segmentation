be0cd570462ab391e9a1323a2e1e552d5984217f
zhuemann <56049645+zhuemann@users.noreply.github.com>
2024-12-01 22:50:42 -0600

    Try making labels for external set

--- external_test_set_creation/external_make_label_from_point.py
+++ external_test_set_creation/external_make_label_from_point.py
@@ -392,7 +392,7 @@ def make_labels_from_suv_max_points():
         #         '/mnt/Bradshaw/UW_PET_Data/raw_nifti_uw_pet/' + save_location + "/" + row["Label_Name"] + '.nii.gz')
         save_location = "testv1"
         nib.save(new_nifti_img,
-                 '/mnt/Bradshaw/UW_PET_Data/external_raw_pet/' + save_location + "/" + row["Label_Name"] + '.nii.gz')
+                 '/mnt/Bradshaw/UW_PET_Data/external_raw_pet/' + save_location + "/" + row["Petlymph"] + '.nii.gz')
     print(f"missing petlymph number: {missing_conversion}")
     print(f"exit early: {exit_early}")
 
