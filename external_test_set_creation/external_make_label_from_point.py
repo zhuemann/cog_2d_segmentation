4e02d247ecc0446846fd8502589f083e05ac30cc
zhuemann <56049645+zhuemann@users.noreply.github.com>
2024-12-01 22:48:32 -0600

    Try making labels for external set

--- external_test_set_creation/external_make_label_from_point.py
+++ external_test_set_creation/external_make_label_from_point.py
@@ -281,7 +281,7 @@ def single_component(original_contour, start_point):
     return component_mask
 
 
-def make_labels_from_suv_max_points(df, save_location):
+def make_labels_from_suv_max_points():
     df = pd.read_excel("/UserData/Zach_Analysis/suv_slice_text/swedish_hospital_external_data_set/swedish_dataframe_max_pixels_v6.xlsx")
     missing_conversion = 0
     petlymph_dic = {}
@@ -390,6 +390,7 @@ def make_labels_from_suv_max_points(df, save_location):
         # nib.save(new_nifti_img, '/UserData/Zach_Analysis/petlymph_image_data/' + save_location +"/"+ row["Label_Name"] + '.nii.gz')
         #nib.save(new_nifti_img,
         #         '/mnt/Bradshaw/UW_PET_Data/raw_nifti_uw_pet/' + save_location + "/" + row["Label_Name"] + '.nii.gz')
+        save_location = "testv1"
         nib.save(new_nifti_img,
                  '/mnt/Bradshaw/UW_PET_Data/external_raw_pet/' + save_location + "/" + row["Label_Name"] + '.nii.gz')
     print(f"missing petlymph number: {missing_conversion}")
