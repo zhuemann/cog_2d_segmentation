4545aa29ff80a8fcb6b0c2d482cc94db7e196067
zhuemann <56049645+zhuemann@users.noreply.github.com>
2024-12-09 00:22:20 -0600

    make plots for v13

--- external_test_set_creation/external_get_max_pixel.py
+++ external_test_set_creation/external_get_max_pixel.py
@@ -10,8 +10,8 @@ from scipy.ndimage import zoom
 
 
 def get_threshold(source):
-    background = 2
-    #background = 1
+    #background = 2
+    background = 1
     new_threshold = (.617*(background/source) + .316)*source
     return new_threshold
 
