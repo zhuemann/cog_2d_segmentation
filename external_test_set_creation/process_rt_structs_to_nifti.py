bbfa7252cc8d6f410fbcfb65e6629014f34f8409
zhuemann <56049645+zhuemann@users.noreply.github.com>
2024-12-12 17:49:35 -0600

    Try coverting rt structs to nifti

--- external_test_set_creation/process_rt_structs_to_nifti.py
+++ external_test_set_creation/process_rt_structs_to_nifti.py
@@ -41,7 +41,8 @@ def process_rt_strcuts_to_nifty():
 
     dicom_location_base = "/UserData/Zach_Analysis/physican_labeling_UWPET/dicom_folders/"
 
-    folder_list = "/UserData/Zach_Analysis/physican_labeling_UWPET/rt_structs_meg_structured/"
+    rt_location = "/UserData/Zach_Analysis/physican_labeling_UWPET/rt_structs_meg_structured/"
+    folder_list = os.listdir(rt_location)
     nifti_save_path = "/UserData/Zach_Analysis/physican_labeling_UWPET/meg_nifti/"
     # Compile a regex pattern to match the desired parts of the filename
     pattern = re.compile(r'UWPETCTWB\.(\d+)_UWPETCTWB\.\1_RTst_(\d{4}-\d{2}-\d{2})')
@@ -73,7 +74,8 @@ def process_rt_strcuts_to_nifty():
         print(f"order_index: {order_index}")
         folder_name = get_folder_by_index(folder_list = folder_list, input_string = patient_id, index = order_index)
         print(f"folder name: {folder_name}")
-        dicom_path_RT_folder = os.path.join(folder_list, folder_name)
+        dicom_path_RT_folder = os.path.join(rt_location, folder_name)
+
         dicom_file_name = os.listdir(dicom_path_RT_folder)
         dicom_path_RT = os.path.join(dicom_path_RT_folder, dicom_file_name)
 
