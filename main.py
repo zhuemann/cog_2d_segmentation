2efe4b86a7e2557942397a3417622b4c5c90df69
zhuemann <56049645+zhuemann@users.noreply.github.com>
2024-12-16 17:18:38 -0600

    copy nifti to folder on pures

--- main.py
+++ main.py
@@ -26,8 +26,8 @@ if __name__ == '__main__':
 
     #process_rt_strcuts_to_nifty()
     #rename_nifti_files()
-    plot_physican_contours()
-    #copy_physican_labels_to_folder()
+    #plot_physican_contours()
+    copy_physican_labels_to_folder()
     print(fail)
     #compound_interactive_report_v2()
     #post_processing_eval()
