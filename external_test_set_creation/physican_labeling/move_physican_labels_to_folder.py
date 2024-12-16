58f4ad74530d7efc1ae83e373e3b834ca23b7beb
zhuemann <56049645+zhuemann@users.noreply.github.com>
2024-12-16 17:24:01 -0600

    copy nifti to folder on pures

--- external_test_set_creation/physican_labeling/move_physican_labels_to_folder.py
+++ external_test_set_creation/physican_labeling/move_physican_labels_to_folder.py
@@ -4,15 +4,15 @@ import pandas as pd
 
 def copy_physican_labels_to_folder():
 
-    #df = pd.read_excel("/UserData/Zach_Analysis/physican_labeling_UWPET/Meghan_worksheet_matched.xlsx")
-    df = pd.read_excel("/UserData/Zach_Analysis/physican_labeling_UWPET/Steve_worksheet_matched.xlsx")
+    df = pd.read_excel("/UserData/Zach_Analysis/physican_labeling_UWPET/Meghan_worksheet_matched.xlsx")
+    #df = pd.read_excel("/UserData/Zach_Analysis/physican_labeling_UWPET/Steve_worksheet_matched.xlsx")
     # Define the base folder for the new location
-    #new_location_folder = "/mnt/Bradshaw/UW_PET_Data/physican_labels/meghan_labels/"
-    new_location_folder = "/mnt/Bradshaw/UW_PET_Data/physican_labels/steve_labels/"
+    new_location_folder = "/mnt/Bradshaw/UW_PET_Data/physican_labels/meghan_labels/"
+    #new_location_folder = "/mnt/Bradshaw/UW_PET_Data/physican_labels/steve_labels/"
 
 
-    #original_location_base = "/UserData/Zach_Analysis/physican_labeling_UWPET/meg_nifti_v2/"
-    original_location_base = "/UserData/Zach_Analysis/physican_labeling_UWPET/steve_nifti/"
+    original_location_base = "/UserData/Zach_Analysis/physican_labeling_UWPET/meg_nifti_v2/"
+    #original_location_base = "/UserData/Zach_Analysis/physican_labeling_UWPET/steve_nifti/"
 
     # Iterate through the dataframe and copy files with new names
     for index, row in df.iterrows():
