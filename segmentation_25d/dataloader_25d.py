56378910e60073e43c8c605e61e54667d62b4ae6
zhuemann <56049645+zhuemann@users.noreply.github.com>
2024-12-20 02:20:37 -0600

    Try training, still not fully functional though

--- segmentation_25d/dataloader_25d.py
+++ segmentation_25d/dataloader_25d.py
@@ -298,7 +298,7 @@ class TextImageDataset(Dataset):
 
         # Stack sagittal and coronal along channel dimension (H, W, 2)
         image = np.stack((img_sag_raw, img_cor_raw), axis=-1)
-        print(f"dataloader image right after loading: {image.shape}")
+        #print(f"dataloader image right after loading: {image.shape}")
 
         # Load label (sagittal label)
         label_path = os.path.join(self.label_path_sagittal, str(self.targets[index]) + "_sag.png")
@@ -312,8 +312,7 @@ class TextImageDataset(Dataset):
 
         # Stack sagittal and coronal labels along channel dimension (H, W, 2)
         segmentation_mask = np.stack((label_sag, label_cor), axis=-1)
-        print(f"dataloader label right after loading: {segmentation_mask.shape}")
-
+        #print(f"dataloader label right after loading: {segmentation_mask.shape}")
 
         if self.transforms is not None:
             # Apply the same transforms to the stacked image and mask
