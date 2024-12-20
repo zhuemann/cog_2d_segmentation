cb0b010db6d601e89cd9ba9280d6d5e98ab97c7c
zhuemann <56049645+zhuemann@users.noreply.github.com>
2024-12-20 17:48:25 -0600

    Try getting correct number out of testing

--- segmentation_25d/train_segmentation_25d.py
+++ segmentation_25d/train_segmentation_25d.py
@@ -722,9 +722,9 @@ def train_image_text_segmentation(config, batch_size=8, epoch=1, dir_base = "/ho
             targets = torch.squeeze(targets)
             images = data['images'].to(device, dtype=torch.float)
             row_ids = data['row_ids']
-            sum_channel_0 = torch.sum(targets[:, 0], dim=(1, 2))
-            sum_channel_1 = torch.sum(targets[:, 1], dim=(1, 2))
-            print(f"channel 0 sum: {sum_channel_0} channel 1 sum: {sum_channel_1}")
+            #sum_channel_0 = torch.sum(targets[:, 0], dim=(1, 2))
+            #sum_channel_1 = torch.sum(targets[:, 1], dim=(1, 2))
+            #print(f"channel 0 sum: {sum_channel_0} channel 1 sum: {sum_channel_1}")
             #print(data)
             #print(data.keys())
             sentences = data['sentence']
