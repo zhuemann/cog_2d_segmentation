7ebe6ff491ef8bc3293a2517086a2825f5a059cc
zhuemann <56049645+zhuemann@users.noreply.github.com>
2024-12-20 14:38:28 -0600

    Try getting correct number out of testing

--- segmentation_25d/train_segmentation_25d.py
+++ segmentation_25d/train_segmentation_25d.py
@@ -759,7 +759,11 @@ def train_image_text_segmentation(config, batch_size=8, epoch=1, dir_base = "/ho
                 print(f"targets size: {targets.size()}")
                 print(f"output size: {outputs.size()}")
 
+                print(f"input to max value images: {images[i][0].size()}")
                 print(f"input to max value targets: {targets[i][0].size()}")
+                print(f"input to max value outputs: {outputs[i][0].size()}")
+                print(f"type target: {type(targets[i][0])}")
+
                 max_target_sagittal, max_output_sagittal = get_max_pixel_value(images[i][0], targets[i][0], outputs[i][0])
                 max_target_coronal, max_output_coronal = get_max_pixel_value(images[i][1], targets[i][1], outputs[i][1])
 
