ea7f03eb85dbd7d56479b126d0b68e9193597c6d
zhuemann <56049645+zhuemann@users.noreply.github.com>
2024-12-22 16:39:29 -0600

    doing 2.5d with two channel input and single channel output

--- main.py
+++ main.py
@@ -115,7 +115,7 @@ if __name__ == '__main__':
 
     config = {"seed": 1, "batch_size": 16, "dir_base": directory_base, "epochs": 250, "n_classes": 2, "LR": 1e-3,
               "IMG_SIZE": (192, 352), "train_samples": .8, "test_samples": .5, "data_path": "D:/candid_ptx/", #"IMG_SIZE": (128, 256)
-              "report_gen": False, "mlm_pretraining": False, "contrastive_training": False, "save_location": "/UserData/Zach_Analysis/result_logs/visual_grounding/25D_experiments/two_channel_input_sagittal/seed1/"}
+              "report_gen": False, "mlm_pretraining": False, "contrastive_training": False, "save_location": "/UserData/Zach_Analysis/result_logs/visual_grounding/25D_experiments/two_channel_input_coronal/seed1/"}
 
 
     acc, valid_log, correct_suv_log, max_predictions = train_image_text_segmentation_25d(config)
--- segmentation_25d/dataloader_25d.py
+++ segmentation_25d/dataloader_25d.py
@@ -310,7 +310,7 @@ class TextImageDataset(Dataset):
 
         # Stack sagittal and coronal labels along channel dimension (H, W, 2)
         #segmentation_mask = np.stack((label_sag, label_cor), axis=-1)
-        segmentation_mask = label_sag
+        segmentation_mask = label_cor
         #print(f"dataloader label right after loading: {segmentation_mask.shape}")
         #print(f"sagital label sum: {np.sum(label_sag)}")
         #print(f"coronal label sum: {np.sum(label_cor)}")
