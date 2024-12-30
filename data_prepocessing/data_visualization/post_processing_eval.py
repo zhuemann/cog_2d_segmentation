1a942db8c2e79347ba63423c5ea987ffe0427dad
zhuemann <56049645+zhuemann@users.noreply.github.com>
2024-12-30 02:04:25 -0600

    try running 3d predictions

--- data_prepocessing/data_visualization/post_processing_eval.py
+++ data_prepocessing/data_visualization/post_processing_eval.py
@@ -644,7 +644,8 @@ def post_processing_eval():
         #prediction_data = resize_3d_prediction(prediction_data, label_data.shape)
         label_data = resize_3d_prediction(label_data, prediction_data.shape)
 
-        #print(f"prediction_data shape: {prediction_data.shape}")
+        print(f"label size: {label_data.shape}")
+        print(f"prediction_data shape: {prediction_data.shape}")
         #print(f"prediction sum: {np.sum(prediction_data)}")
         #print(f"label sum: {np.sum(label_data)}")
         #prediction_data = filter_prediction_by_average(prediction_data)
