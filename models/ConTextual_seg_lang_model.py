cff559d7dae897d19a1a6e6ccd96ffff7f5d846e
zhuemann <56049645+zhuemann@users.noreply.github.com>
2024-12-30 20:12:42 -0600

    Starting two channel saggital v2

--- models/ConTextual_seg_lang_model.py
+++ models/ConTextual_seg_lang_model.py
@@ -65,7 +65,7 @@ class Attention_ConTEXTual_Lang_Seg_Model(torch.nn.Module):
         lang_rep = word_rep
 
         # repeats the pooled output from roberta and uses it as the langauge output
-        lang_rep = report_rep.unsqueeze(1).repeat(1, word_rep.size(1), 1)
+        #lang_rep = report_rep.unsqueeze(1).repeat(1, word_rep.size(1), 1)
 
         # for t5
         #encoder_output = self.lang_encoder.encoder(input_ids=ids, attention_mask=mask, return_dict=True)
