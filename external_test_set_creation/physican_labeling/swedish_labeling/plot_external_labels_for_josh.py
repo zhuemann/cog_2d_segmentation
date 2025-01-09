d3df59b5874cf06f96e302c84744610596267969
zhuemann <56049645+zhuemann@users.noreply.github.com>
2025-01-08 22:42:24 -0600

    Try plotting cropped images for josh

--- external_test_set_creation/physican_labeling/swedish_labeling/plot_external_labels_for_josh.py
+++ external_test_set_creation/physican_labeling/swedish_labeling/plot_external_labels_for_josh.py
@@ -382,7 +382,7 @@ def plot_final_testset_for_josh_v3_external():
         plt.suptitle(sentence, fontsize=12, color='black')
 
         plt.savefig(
-            "/UserData/Zach_Analysis/physican_labeling_UWPET/swedish_labeled_dataset/external_plots_for_josh/" + label_name,
+            "/UserData/Zach_Analysis/physican_labeling_UWPET/swedish_labeled_dataset/external_plots_for_josh/" + label_name + ".png",
             dpi=300)
 
         plt.close()
