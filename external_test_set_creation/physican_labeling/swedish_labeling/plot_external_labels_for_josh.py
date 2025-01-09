a6f2d04169e3fb62f29ddb0c04daf867219cc8e8
zhuemann <56049645+zhuemann@users.noreply.github.com>
2025-01-09 15:36:07 -0600

    Try plotting cropped images for josh

--- external_test_set_creation/physican_labeling/swedish_labeling/plot_external_labels_for_josh.py
+++ external_test_set_creation/physican_labeling/swedish_labeling/plot_external_labels_for_josh.py
@@ -84,7 +84,10 @@ def plot_final_testset_for_josh_v3_external():
     for index, row in df.iterrows():
         print(f"index: {index}")
         i += 1
-        if i < 190:
+        if i < 195:
+            continue
+        if pd.isna(row["ID"]):
+            print("nan continue")
             continue
         petlymph = extract_image_id(row["ID"])
         print(petlymph)
